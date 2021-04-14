ea6fbbccc64910345ea33b69044dded7a4dac68a
Jelani Thomas <jelani13@icloud.com>
2021-04-14 02:42:41 -0400

    removed unnessary files. Footer compponent overhaul

---
+++ client/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <div>
+            <ul>
+              return (
+              <li>
+                <a href>
+                  <i />
+                </a>
+              </li>
+              )
+            </ul>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
