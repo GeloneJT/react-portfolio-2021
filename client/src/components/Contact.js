a054c017e473c1f10df74eca5436e3d32e906e37
Jelani Thomas <jelani13@icloud.com>
2021-04-14 06:34:48 -0400

    contact.js framework created

---
+++ client/src/components/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <div>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <aside>
+            <div>
+              <h4></h4>
+            </div>
+          </aside>
+        </div>
+      </section>
+    );
+  }
+}
