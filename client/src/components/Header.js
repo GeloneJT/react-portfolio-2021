4a59d6c7499a30d997b01929a3994eb87851af2e
Jelani Thomas <jelani13@icloud.com>
2021-04-14 02:56:08 -0400

    Header framework done

---
+++ client/src/components/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <nav>
+            <a href></a>
+            <a href></a>
+            <ul>
+              <li>
+                <a href></a>
+              </li>
+              <li>
+                <a href></a>
+              </li>
+              <li>
+                <a href></a>
+              </li>
+              <li>
+                <a href></a>
+              </li>
+              <li>
+                <a href></a>
+              </li>
+              <li>
+                <a href></a>
+              </li>
+            </ul>
+          </nav>
+
+          <div>
+            <div>
+              <h1></h1>
+              <h3></h3>
+              <hr />
+              <ul>
+                <li>
+                  <a href></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <p>
+            <a href>
+              <i></i>
+            </a>
+          </p>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
