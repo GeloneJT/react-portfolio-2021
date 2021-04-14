7ce83ef0e67c793edc0daf687b6c2ae548da46e6
Jelani Thomas <jelani13@icloud.com>
2021-04-14 07:07:22 -0400

    Resume.js component created

---
+++ client/src/components/Resume.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+export default class Resume extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <div>
+            <h1></h1>
+          </div>
+
+          <div>
+            return(
+            <div>
+              <div>
+                <h3></h3>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+            )
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1></h1>
+          </div>
+
+          <div>
+            return(
+            <div>
+              <div>
+                <h3></h3>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+            )
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h1></h1>
+          </div>
+
+          <div>
+            <p></p>
+            <div>
+              <ul>
+                return (<li></li>)
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
