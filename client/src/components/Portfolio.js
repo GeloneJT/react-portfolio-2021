1e182b6baec7c7805d938a071efbeea7d97183f1
Jelani Thomas <jelani13@icloud.com>
2021-04-14 06:51:20 -0400

    Portfolio.js framework created

---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class Portfolio extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <div>
+            <h1></h1>
+            <div>
+              return (
+              <div>
+                <div>
+                  <a href>
+                    <img />
+                    <div>
+                      <div>
+                        <h5></h5>
+                        <p></p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              )
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
