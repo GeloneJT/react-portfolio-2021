ed22fadfa958248fc7a73835aa4fbe1e47450ad7
Jelani Thomas <jelani13@icloud.com>
2021-04-14 07:18:48 -0400

    portfolio 'crust' completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 // import './App.css';
-import Footer from "./components/Footer"
+import { Component } from "react";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
-function App() {
-  return (
-    <main>
-      <Footer />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
