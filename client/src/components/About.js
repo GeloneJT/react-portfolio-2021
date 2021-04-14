861677eec946260735e4a43fb013616b8e9e7f25
Jelani Thomas <jelani13@icloud.com>
2021-04-14 06:19:05 -0400

    about.js framework created

---
+++ client/src/components/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <div>
+            <img />
+          </div>
+
+          <div>
+            <h2></h2>
+            <p></p>
+
+            <div>
+              <div>
+                <h2></h2>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
